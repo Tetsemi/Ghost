a040b068fd10f476fb88648b70abc085d66edbb2
Tetsemi <ivorytiger@gmail.com>
2026-03-14 22:09:36 -0700

    Updated Theming

--- Templates/file size sha256.ps1
+++ Templates/file size sha256.ps1
@@ -6,3 +6,7 @@ Write-Output ""
 Write-Output "File: ghost_of_arcadia.css"
 Write-Output "Size: $((Get-Item -Path 'E:\Users\ivory\Documents\GitHub\Ghost\ghost_of_arcadia.css').Length)"
 Get-FileHash E:\Users\ivory\Documents\GitHub\Ghost\ghost_of_arcadia.css | ForEach-Object { "$($_.Algorithm): $($_.Hash)" }
+Write-Output ""
+Write-Output "File: translation.json"
+Write-Output "Size: $((Get-Item -Path 'E:\Users\ivory\Documents\GitHub\Ghost\translation.json').Length)"
+Get-FileHash E:\Users\ivory\Documents\GitHub\Ghost\translation.json | ForEach-Object { "$($_.Algorithm): $($_.Hash)" }

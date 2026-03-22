8e2f72bfe76dc435bec56fc03c90560402cbe53d
Tetsemi <ivorytiger@gmail.com>
2026-03-22 11:17:09 -0700

    Updating project files

---
+++ Documentation/DataMaps/conditionsDataMap.js
@@ -0,0 +1,128 @@
+const conditionsDataMap = {
+	blind: {
+		name_key:      "condition_blind-u",
+		rule_text_key: "condition_blind_rules-u",
+		category:      "combat",
+		duration:      "source_defined",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	burning: {
+		name_key:      "condition_burning-u",
+		rule_text_key: "condition_burning_rules-u",
+		category:      "combat",
+		duration:      "ongoing",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	chilled: {
+		name_key:      "condition_chilled-u",
+		rule_text_key: "condition_chilled_rules-u",
+		category:      "combat",
+		duration:      "next_turn",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	deafened: {
+		name_key:      "condition_deafened-u",
+		rule_text_key: "condition_deafened_rules-u",
+		category:      "combat",
+		duration:      "source_defined",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	disoriented: {
+		name_key:      "condition_disoriented-u",
+		rule_text_key: "condition_disoriented_rules-u",
+		category:      "combat",
+		duration:      "source_defined",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	entangled: {
+		name_key:      "condition_entangled-u",
+		rule_text_key: "condition_entangled_rules-u",
+		category:      "combat",
+		duration:      "until_escaped",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	exhausted: {
+		name_key:      "condition_exhausted-u",
+		rule_text_key: "condition_exhausted_rules-u",
+		category:      "strain",
+		duration:      "until_strain_recovery",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	hobbled: {
+		name_key:      "condition_hobbled-u",
+		rule_text_key: "condition_hobbled_rules-u",
+		category:      "combat",
+		duration:      "next_turn",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	impaired: {
+		name_key:      "condition_impaired-u",
+		rule_text_key: "condition_impaired_rules-u",
+		category:      "combat",
+		duration:      "1_round",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	jammed: {
+		name_key:      "condition_jammed-u",
+		rule_text_key: "condition_jammed_rules-u",
+		category:      "weapon",
+		duration:      "until_cleared",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	panicked: {
+		name_key:      "condition_panicked-u",
+		rule_text_key: "condition_panicked_rules-u",
+		category:      "combat",
+		duration:      "1d3_rounds",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	poisoned: {
+		name_key:      "condition_poisoned-u",
+		rule_text_key: "condition_poisoned_rules-u",
+		category:      "combat",
+		duration:      "until_treated",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	shaken: {
+		name_key:      "condition_shaken-u",
+		rule_text_key: "condition_shaken_rules-u",
+		category:      "combat",
+		duration:      "next_turn",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	slowed: {
+		name_key:      "condition_slowed-u",
+		rule_text_key: "condition_slowed_rules-u",
+		category:      "combat",
+		duration:      "until_removed",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	stunned: {
+		name_key:      "condition_stunned-u",
+		rule_text_key: "condition_stunned_rules-u",
+		category:      "combat",
+		duration:      "next_turn",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	suppressed: {
+		name_key:      "condition_suppressed-u",
+		rule_text_key: "condition_suppressed_rules-u",
+		category:      "combat",
+		duration:      "next_turn",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	tired: {
+		name_key:      "condition_tired-u",
+		rule_text_key: "condition_tired_rules-u",
+		category:      "strain",
+		duration:      "until_strain_recovery",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	},
+	weakened: {
+		name_key:      "condition_weakened-u",
+		rule_text_key: "condition_weakened_rules-u",
+		category:      "combat",
+		duration:      "until_treated",
+		source: { doc: "combat_conditions-u", version: "2.260315", date: "2026-03-15", section: "conditions-u" }
+	}
+};

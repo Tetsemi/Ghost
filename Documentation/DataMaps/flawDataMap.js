b8addef03c5f25c1f96a7a57ce204a8133cc8b30
Tetsemi <ivorytiger@gmail.com>
2026-03-22 11:50:46 -0700

    Updated Strain Recovery

---
+++ Documentation/DataMaps/flawDataMap.js
@@ -0,0 +1,129 @@
+const flawDataMap = {
+	addiction_major: {
+		name: "flaw_addiction_major-u",
+		flavor: "flaw_addiction_major_flavor-u",
+		description: "flaw_addiction_major_description-u",
+		cost: 10,
+		affected_skill: [ "insight" ],
+		affected_stat: "pow",
+		usage_limit: "",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	addiction_minor: {
+		name: "flaw_addiction_minor-u",
+		flavor: "flaw_addiction_minor_flavor-u",
+		description: "flaw_addiction_minor_description-u",
+		cost: 5,
+		affected_skill: [ "insight" ],
+		affected_stat: "pow",
+		usage_limit: "",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	blood_debt: {
+		name: "flaw_blood_debt-u",
+		flavor: "flaw_blood_debt_flavor-u",
+		description: "flaw_blood_debt_description-u",
+		cost: 10,
+		usage_limit: "session",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	burned_bridges: {
+		name: "flaw_burned_bridges-u",
+		flavor: "flaw_burned_bridges_flavor-u",
+		description: "flaw_burned_bridges_description-u",
+		cost: 5,
+		usage_limit: "session",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	compulsive_gambler: {
+		name: "flaw_compulsive_gambler-u",
+		flavor: "flaw_compulsive_gambler_flavor-u",
+		description: "flaw_compulsive_gambler_description-u",
+		cost: 5,
+		affected_stat: "pow",
+		usage_limit: "",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	cyber_rejection: {
+		name: "flaw_cyber_rejection-u",
+		flavor: "flaw_cyber_rejection_flavor-u",
+		description: "flaw_cyber_rejection_description-u",
+		cost: 5,
+		affected_stat: "vitality",
+		usage_limit: "",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	distinctive_look: {
+		name: "flaw_distinctive_look-u",
+		flavor: "flaw_distinctive_look_flavor-u",
+		description: "flaw_distinctive_look_description-u",
+		cost: 5,
+		usage_limit: "session",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	hex_stained: {
+		name: "flaw_hex_stained-u",
+		flavor: "flaw_hex_stained_flavor-u",
+		description: "flaw_hex_stained_description-u",
+		cost: 10,
+		usage_limit: "session",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	hunted: {
+		name: "flaw_hunted-u",
+		flavor: "flaw_hunted_flavor-u",
+		description: "flaw_hunted_description-u",
+		cost: 10,
+		usage_limit: "session", //GM may introduce a threat once per session
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	marked_by_the_black: {
+		name: "flaw_marked_by_the_black-u",
+		flavor: "flaw_marked_by_the_black_flavor-u",
+		description: "flaw_marked_by_the_black_description-u",
+		cost: 10,
+		usage_limit: "session",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	mental_scar: {
+		name: "flaw_mental_scar-u",
+		flavor: "flaw_mental_scar_flavor-u",
+		description: "flaw_mental_scar_description-u",
+		cost: 10,
+		affected_stat: "pow",
+		usage_limit: "",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	obligation: {
+		name: "flaw_obligation-u",
+		flavor: "flaw_obligation_flavor-u",
+		description: "flaw_obligation_description-u",
+		cost: 10,
+		usage_limit: "session",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	soul_flare: {
+		name: "flaw_soul_flare-u",
+		flavor: "flaw_soul_flare_flavor-u",
+		description: "flaw_soul_flare_description-u",
+		cost: 10,
+		usage_limit: "scene", //once per scene surge check
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	veil_touched: {
+		name: "flaw_veil_touched-u",
+		flavor: "flaw_veil_touched_flavor-u",
+		description: "flaw_veil_touched_description-u",
+		cost: 5,
+		usage_limit: "",
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	},
+	wanted_low_priority: {
+		name: "flaw_wanted_low_priority-u",
+		flavor: "flaw_wanted_low_priority_flavor-u",
+		description: "flaw_wanted_low_priority_description-u",
+		cost: 5,
+		usage_limit: "session", //GM may introduce interference once per session
+		source: { doc: "perks_and_flaws-u", version: "2.260125", date: "2026-01-25", section: "perks_and_flaws-u" }
+	}
+};
